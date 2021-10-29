72d86fa3efe9f568abfa86a59b84cbaa04cd0ada
Tati <Tatiianacs@gmail.com>
2021-10-29 11:45:01 +0200

    Second commit

--- src/main/java/com/add/Prueba/ConnectionJDBC.java
+++ src/main/java/com/add/Prueba/ConnectionJDBC.java
@@ -16,6 +16,7 @@ public class ConnectionJDBC {
 		
 		//select FIJA
 		ResultSet rs = stmt.executeQuery("Select * from empleados");
+		System.out.println("Hola mundo");
 		//Resulset
 		while (rs.next())
 		    System.out.println (rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) 
